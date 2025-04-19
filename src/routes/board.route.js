9a0b0839f3921adefbb2e285d9a371ac1cf189e9
Aman Bhondge <amanbhondge@gmail.com>
2025-04-19 10:36:21 +0530

    6

--- src/routes/board.route.js
+++ src/routes/board.route.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import validate  from '../middlewares/validate.middleware.js'
+import validate from '../middlewares/validate.middleware.js'
 import { createBoardValidation, updateBoardTitleValidation } from '../validators/board.validator.js';
 import { createBoard, updateBoardTitle } from '../controllers/board.controller.js';
+import authorize from '../middlewares/authorize.middleware.js';
 
 const boardRouter = express.Router();
 
@@ -9,6 +10,7 @@ const boardRouter = express.Router();
 
 boardRouter.post(
     '/create',
+    authorize,
     createBoardValidation,
     validate,
     createBoard
@@ -16,6 +18,7 @@ boardRouter.post(
 
 boardRouter.put(
     '/update/:id/title',
+    authorize,
     updateBoardTitleValidation,
     validate,
     updateBoardTitle
