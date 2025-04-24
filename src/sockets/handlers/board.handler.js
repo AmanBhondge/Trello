7d029253df42bc3fbe56aa3ba4f9ef8bfb63115c
Aman Bhondge <amanbhondge@gmail.com>
2025-04-24 15:31:04 +0530

    53

--- src/sockets/handlers/board.handler.js
+++ src/sockets/handlers/board.handler.js
@@ -1,33 +1,32 @@
-export const handleBoardEvents = (socket, io) => {
-    const { userId, userName } = socket.user;
-  
-    socket.on("joinBoard", (boardId) => {
-      socket.join(boardId);
-      socket.to(boardId).emit("userActive", { userId, userName, boardId, timestamp: new Date() });
+export const handleBoardEvents = (io, socket, onlineUsers) => {
+  const { userId, userName } = socket.user;
+
+  socket.on("joinBoard", (boardId) => {
+    socket.join(boardId);
+    socket.to(boardId).emit("userActive", { userId, userName, boardId, timestamp: new Date() });
+  });
+
+  socket.on("leaveBoard", (boardId) => {
+    socket.leave(boardId);
+    socket.to(boardId).emit("userLeft", { userId, userName, boardId, timestamp: new Date() });
+  });
+
+  socket.on("boardContentUpdate", ({ boardId, content, lastModified }) => {
+    socket.to(boardId).emit("boardContentUpdated", {
+      boardId,
+      content,
+      lastModified,
+      updatedBy: { userId, userName }
     });
-  
-    socket.on("leaveBoard", (boardId) => {
-      socket.leave(boardId);
-      socket.to(boardId).emit("userLeft", { userId, userName, boardId, timestamp: new Date() });
+  });
+
+  socket.on("cursorMove", ({ boardId, position, selection }) => {
+    socket.to(boardId).emit("userCursorMoved", {
+      userId,
+      userName,
+      position,
+      selection,
+      timestamp: new Date()
     });
-  
-    socket.on("boardContentUpdate", ({ boardId, content, lastModified }) => {
-      socket.to(boardId).emit("boardContentUpdated", {
-        boardId,
-        content,
-        lastModified,
-        updatedBy: { userId, userName }
-      });
-    });
-  
-    socket.on("cursorMove", ({ boardId, position, selection }) => {
-      socket.to(boardId).emit("userCursorMoved", {
-        userId,
-        userName,
-        position,
-        selection,
-        timestamp: new Date()
-      });
-    });
-  };
-  
+  });
+};
